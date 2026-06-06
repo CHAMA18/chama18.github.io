67141e2795a3dacd04912bd47d58dfb361b39d5a
CHAMA18 <chungu424@gmail.com>
2026-06-06 19:09:07 +0000

    Update root site with staging-merged code: 2026-06-06 19:09

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3061797826" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1471267309" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });

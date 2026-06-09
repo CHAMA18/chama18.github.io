87a2c9f7a59bcbd3e51eb1f2a5127b5b4ac79175
CHAMA18 <chungu424@gmail.com>
2026-06-09 12:57:18 +0000

    Update root site: 2026-06-09 12:57

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "112221658" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "227360405" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
